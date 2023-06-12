373cca54e1e49383fe04276ac3be548adfc6c109
minhducnguyen189 <minhducnguyen189@gmail.com>
2023-06-12 15:35:22 +0700

    update example for java core static keyword

--- src/main/java/com/java/core/nonmodifier/JavaStaticMain.java
+++ src/main/java/com/java/core/nonmodifier/JavaStaticMain.java
@@ -2,6 +2,11 @@ package com.java.core.nonmodifier;
 
 public class JavaStaticMain {
 
+
+    static {
+        System.out.println("This static block runs before the main method of JavaStaticMain 1");
+    }
+
     public static void main(String[] args) {
         System.out.println("countValue is " + StaticVariableClassExample.count);
         StaticVariableClassExample staticVariable = new StaticVariableClassExample();
@@ -10,4 +15,9 @@ public class JavaStaticMain {
         System.out.println("Sum of a and b: " + StaticMethodClassExample.add(5,5));
         System.out.println("StaticBlockClassExample count Value is: " + StaticBlockClassExample.count);
     }
+
+    static {
+        System.out.println("This static block runs before the main method of JavaStaticMain 2");
+    }
+
 }
