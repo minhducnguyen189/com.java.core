2b2997a53e1c7336842fed4deef661bd6298d3af
ducnguyen <minhducnguyen189@gmail.com>
2024-06-03 21:16:38 +0700

    update example for JavaDateTime

--- src/main/java/com/java/core/datetime/JavaDateTime.java
+++ src/main/java/com/java/core/datetime/JavaDateTime.java
@@ -7,15 +7,27 @@ import java.util.Locale;
 
 public class JavaDateTime {
 
-
     public static void main(String[] args) {
-        String localDateStr = "04-APR-22";
+        System.out.println(getLocalDate("04-APR-22"));
+        System.out.println(getLocalDateTime("04-APR-22"));
+    }
+
+    public static String getLocalDate(String localDateStr) {
         DateTimeFormatter dateTimeFormatter = new DateTimeFormatterBuilder()
                 .parseCaseInsensitive()
                 .appendPattern("dd-MMM-yy")
                 .toFormatter(Locale.US);
         LocalDate localDate = LocalDate.parse(localDateStr, dateTimeFormatter);
-        System.out.printf(localDate.toString());
+        return localDate.toString();
+    }
+
+    public static String getLocalDateTime(String localDateStr) {
+        DateTimeFormatter dateTimeFormatter = new DateTimeFormatterBuilder()
+                .parseCaseInsensitive()
+                .appendPattern("dd-MMM-yy")
+                .toFormatter(Locale.US);
+        LocalDate localDate = LocalDate.parse(localDateStr, dateTimeFormatter);
+        return localDate.atStartOfDay().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
     }
 
 }
