a6740abfedc0a1ee86633f45f240e4334797761b
Duc Nguyen <minhducnguyen189@gmail.com>
2025-05-11 14:58:43 +0700

    learning thread

---
+++ src/main/java/com/java/core/thread/multi_threading_interrupt/LongComputationTaskEx.java
@@ -0,0 +1,40 @@
+package com.java.core.thread.multi_threading_interrupt;
+
+import java.math.BigInteger;
+
+public class LongComputationTaskEx {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new LongComputationTask(new BigInteger("2000"), new BigInteger("1000000000")));
+        thread.start();
+        thread.interrupt();
+    }
+
+    static class LongComputationTask implements Runnable {
+        private final BigInteger base;
+        private final BigInteger power;
+
+        public LongComputationTask(BigInteger base, BigInteger power) {
+            this.base = base;
+            this.power = power;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(base+"^"+power+" = " + pow(base, power));
+        }
+
+        private BigInteger pow(BigInteger base, BigInteger power) {
+            BigInteger result = BigInteger.ONE;
+            for (BigInteger i = BigInteger.ZERO; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)) {
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println("Prematurely interrupted computation");
+                    return BigInteger.ZERO;
+                }
+                result = result.multiply(base);
+            }
+            return result;
+        }
+    }
+
+}
